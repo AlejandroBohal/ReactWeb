3a5de92ba3b7a34a73cc0da13b88ddb3be398096
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-12-19 00:51:53 -0500

    Components,props, useState

---
+++ 03-CounterApp/src/CounterApp.js
@@ -0,0 +1,29 @@
+//rafcp rafce rafc snnipet
+//events React inician on On
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+
+const CounterApp = ({value = 10}) => {
+
+    const [counter,setCounter] = useState(0); // [counter,setCounter]
+    
+    // handleAdd
+    const handleAdd = () => setCounter( counter+1 ); //setCounter ( (counter) => counter+1);
+    const handleSubstract = () => setCounter( counter-1 );
+    const handleReset = () => setCounter( value );
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2>{counter}</h2>
+            {/* <button onClick={ (e) => handleAdd(e)}> </button> */}
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubstract }>-1</button>
+        </>
+    );
+}
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+ 
+export default CounterApp;
---
+++ 03-CounterApp/src/PrimeraApp.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// Functional components
+const PrimeraApp = ( {saludo,subtitulo} ) => {
+    
+    return( 
+        <>
+            {/*<pre>{ JSON.stringify(saludo,null,3) }</pre>*/}
+            <h2> {saludo} </h2>
+            <p> {subtitulo}</p>
+            
+        </>
+    );
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired,
+}
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo',
+}
+export default PrimeraApp;
